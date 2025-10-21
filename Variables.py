0455f9f2b5eea549469e99704b02df28cd7acbff
Sinchana M <sinchanam7926@gmail.com>
2025-10-21 15:31:33 +0530

    add code for python learning journal

---
+++ Variables.py
@@ -0,0 +1,19 @@
+name = "Sinchana"
+age = 19
+price = 77.77
+print("name")
+print(name)
+print(age)
+print(price)
+print("My name is ",name)
+print(type(name))
+
+
+#Expression Execution
+A,B=2,3
+Txt="@"
+print(2*Txt*3)
+
+#Input in python 
+name = input("name:")
+print(name)
