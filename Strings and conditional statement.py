674201b4789b0fc1e8eb448b051a88ed08e2dcbe
Sinchana M <sinchanam7926@gmail.com>
2025-10-22 15:50:45 +0530

    Add files via upload
    Day 2 : Strings and conditional statement

---
+++ Strings and conditional statement.py
@@ -0,0 +1,31 @@
+#Conditional statement
+#Example
+light = input("Light color:")
+if(light == "red"):
+    print("Stop")
+elif(light == "yellow" ):
+    print("Look")
+elif(light == "Green"):
+    print("Go")
+else:
+    print("Light is broken")
+
+#Strings
+str1 = "Hello "
+str2 = "Guyss "
+str3 = "This is sinchana"
+final_str = str1+str2+str3
+print(final_str)
+
+#Length of string
+str1 = "Hello "
+Length = len(str1)
+print(Length)
+str2 = "Guyss "
+str3 = "This is sinchana"
+final_str = str1+str2+str3
+print(final_str)
+
+ #Slicing
+str3 = "This is sinchana"
+print(str[4]) 
