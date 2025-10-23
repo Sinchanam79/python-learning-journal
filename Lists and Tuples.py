bcd826607bd39ea7f79bbb3fcb722ba4b7d62dd8
Sinchana M <sinchanam7926@gmail.com>
2025-10-23 15:53:07 +0530

    Add files via upload

---
+++ Lists and Tuples.py
@@ -0,0 +1,23 @@
+#Lists in python
+marks1 = 55.5
+marks2 = 66.7
+marks3 = 42.5
+marks4 = 55.5
+
+marks = [55.5,66.7,42.5,55.5]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(len(marks))
+ 
+#Slicing
+print(marks[1:2])
+
+#List methods
+list =[2,3,4]
+list.append(5)
+list.sort()
+list.sort(reverse=True)
+list.reverse()
+list.insert(0,8)
+
