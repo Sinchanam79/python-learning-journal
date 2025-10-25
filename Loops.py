70848d2661c679490e49839cd11186666809fea6
Sinchana M <sinchanam7926@gmail.com>
2025-10-25 19:25:46 +0530

    Day 5

---
+++ Loops.py
@@ -0,0 +1,21 @@
+#Loops in python
+#While
+#while.condition :
+count = 1
+while count <= 2: #stopping condition
+    print("Hello")
+    count+=1
+print(count)
+
+#Multiplication of any number
+n = int(input("enter a numner :"))
+i = 1
+while i <=10:
+    print(n*i)
+    i+=1
+print(i)
+
+
+
+ 
+
